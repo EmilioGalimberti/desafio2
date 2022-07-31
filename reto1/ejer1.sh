fa5dcf46ae1816341a0e32c01c6abdbead9f5700
emily <emiliogalimberty@gmail.com>
2022-07-31 15:59:45 -0300

    +ADD: Reto1/Ejercio 3 listo

--- reto1/ejer1.sh
+++ reto1/ejer1.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+read mensaje1
+
+
+if [ -z "$mensaje1" ]
+then
+    mensaje1=" Que me gusta bash!!"
+fi
+
+
 mkdir foo;
-mkdir dummy;
-touch dummy/file2.txt;
-echo Me encanta bash!! > dummy/file1.txt;
-mkdir empty
+mkdir foo/dummy;
+echo $mensaje1  > foo/dummy/file1.txt;
+mkdir foo/empty;
+echo $mensaje1 >  foo/empty/file2.txt;
+
 
 
